f024a1d7f95442f009b53469530c6c6c7ab6b113
Justin Chou <j8chou@ucsd.edu>
2017-05-16 01:58:59 -0700

    Update config to point mongo to mlab

--- server/config/config.js
+++ server/config/config.js
@@ -2,7 +2,7 @@
  * Config var for app
 **/
 module.exports = {
-  mongoDBUrl: process.env.MONGODB_URL || process.env.MONGOLAB_URI  || 'mongodb://localhost:27017/webstormtroopers',
+  mongoDBUrl: process.env.MONGODB_URL || process.env.MONGOLAB_URI  || 'mongodb://root:qwerty@ds143211.mlab.com:43211/emissary',
   port: process.env.PORT || 4941,
   secret: process.env.SECRET || 'mysecret'
 };
