e4ad378aa3052417934ecf3072931516b9788e24
Ziqing Guo <zack.ziqing.guo@gmail.com>
2017-05-22 15:59:06 -0700

    Update e2e.js

--- test/e2e/e2e.js
+++ test/e2e/e2e.js
@@ -1,19 +1,103 @@
 module.exports = {
 	'E2E Emissary Testing': function(client){
 		client
-			.url('http://http://35.185.226.210/')
-			.useXpath()
-			//.click("//a[text()='Log In']")
-			.click("")
-			.useCss()
+			.url('http://35.185.226.210')
 			.waitForElementVisible('body',1000)
-			.assert.visible('input[name=email]')
-			.setValue('input[name=email]', 'peter@enque.com')
-			.assert.visible('input[name=password]')
-			.setValue('input[name=password]', 'peter')
-			.click('button[type=submit]')
+			.assert.title('Emissary')
+			.assert.attributeContains('a','href','http://35.185.226.210/index.html')
+			.assert.attributeContains('nav','class','site-nav')
+			.pause(1000)
+
+			//testing sign up page
+			.click("div.callout-button")
 			.pause(3000)
-			.assert.title('Express')
+			.assert.attributeContains('body','id',"sign-up-body")
+			.assert.visible('input[name=name]')
+			.setValue('input[name=name]','666666')
+			.assert.visible('input[name=email]')
+			.setValue('input[name=email]','testing11@test.com')
+			.assert.visible('input[name=phone_number]')
+			.setValue('input[name=phone_number]','8880008888')
+			.pause(2000)
+
+
+			.assert.visible('button[id=submit-company-btn]')
+			.click('button[id=submit-company-btn]')
+			.pause(2000)
+
+			.assert.visible('input[name=first]')
+			.setValue('input[name=first]','testFirst')
+
+			.assert.visible('input[name=last]')
+			.setValue('input[name=last]','testLast')
+
+			.assert.visible('input[id=form-employee-email]')
+			.setValue('input[id=form-employee-email]','testing11@test.com')
+
+			.assert.visible('input[id=form-employee-phone]')
+			.setValue('input[id=form-employee-phone]','8888888888')
+
+			.assert.visible('input[name=form-password]')
+			.setValue('input[name=form-password]','12345678')
+
+			.assert.visible('input[name=form-repeat-password]')
+			.setValue('input[name=form-repeat-password]','12345678')
+			.pause(2000)
+
+			.click('button[id=submit-btn]')
+			.waitForElementVisible('body',1000)
+			.assert.title('Login')
+			.pause(1000)
+			.assert.visible("div#visitor-queue")
+			.pause(3000)
+			//finish testing signing up
+
+			.url('http://35.185.226.210/login.html')
+			.waitForElementVisible('body',1000)
+			.assert.title('Emissary | Login')
+			.pause(2000)
+
+			.assert.visible("input#username.form-control")//'input[id=username]')
+			.setValue("input#username.form-control",'testing@test.com')
+			.assert.visible("input#password.form-control")//'input[id=password]')
+			.setValue("input#password.form-control",'1234567')
+			.click('button[id=loginButton]')
+			.pause(1000)
+			.assert.visible("div#errorlog.error")
+			.pause(2000)
+
+			.assert.visible("input#username.form-control")
+			.clearValue("input#username.form-control")
+			.pause(500)
+			.setValue("input#username.form-control",'testing@test.com')
+			.assert.visible("input#password.form-control")
+			.clearValue("input#password.form-control")
+			.pause(500)
+			.setValue("input#password.form-control",'12345678')
+			.assert.visible('button[id=loginButton]')
+			.click('button[id=loginButton]')
+			.pause(2000)
+
+			.waitForElementVisible('body',2000)
+			.assert.title('Emissary | Visitors')
+			.assert.visible("div#visitor-queue")
+			.pause(1000)
+
+			.url("http://35.185.226.210/employees.html")
+			.assert.visible("div.employee-container")
+			.pause(2000)
+
+			.url("http://35.185.226.210/appointments.html")
+			.assert.visible("div.add-button")
+			.pause(2000)
+
+			.url("http://35.185.226.210/form-builder.html")
+			.assert.visible("form.form-builder")
+			.pause(2000)
+
+			.url("http://35.185.226.210/settings.html")
+			.assert.visible("div.main-content")
+			.pause(2000)
 			.end();
 	}
 };
