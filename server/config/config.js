ceec9415e05143aa51ef4b5110a614a499a05b9e
david <david@ubuntu.ubuntu-domain>
2017-06-10 16:43:58 -0700

    Re-add dashboard to visitors. Change mongo db to make tests pass

--- server/config/config.js
+++ server/config/config.js
@@ -2,7 +2,7 @@
  * Config var for app
 **/
 module.exports = {
-  mongoDBUrl: process.env.MONGODB_URL || process.env.MONGOLAB_URI  || 'mongodb://root:qwerty@ds143211.mlab.com:43211/emissary',
+  mongoDBUrl: process.env.MONGODB_URL || process.env.MONGOLAB_URI  || 'mongodb://root:qwerty@ds121192.mlab.com:21192/emissary',
   port: process.env.PORT || 4941,
   secret: process.env.SECRET || 'mysecret'
 };
